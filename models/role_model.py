88fcf84972e78877fe76ccc9b11352cf17fa7962
theus-figueiredo <matheus.b.figueiredo@outlook.com>
2023-04-04 15:57:58 -0300

    feito o base da model de role, service_category e service_status

---
+++ models/role_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from core.configs import Settings
+
+class RoleModel(Settings.DB_BASE_MODEL):
+    __tablename__ = 'roles'
+    __allow_unmapped__ = True
+    
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    role=Column(String(24))
